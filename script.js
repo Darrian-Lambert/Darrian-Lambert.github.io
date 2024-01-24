485fa41371f7502597d366416f798295d1a72cb8
Darrian Lambert <Dtlambert1@buffs.wtamu.edu>
2024-01-24 15:13:28 -0600

    JS 1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 let sMenu = document.getElementById("menu-icon");
+let menu = document.getElementById("menu");
+
+sMenu.addEventListener("click", function() {
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block"
+    }
+});
