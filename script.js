8b76de6add0da2b081f20eb2fc7d177d3ebfe2e2
Darrian Lambert <Dtlambert1@buffs.wtamu.edu>
2024-01-29 13:34:43 -0600

    button one

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let sMenu = document.getElementById("menu-icon");
 let menu = document.getElementById("menu");
+let menuHeight = menu.clientHeight;
+let head = document.getElementById("MG");
 
 sMenu.addEventListener("click", function() {
     if (menu.style.display === "block") {
         menu.style.display = "none";
+        head.style.height = "120px";
     } else {
-        menu.style.display = "block"
+        menu.style.display = "block";
+        head.style.height = (menuHeight + 10) + "px";
     }
 });
